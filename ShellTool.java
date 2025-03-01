f879e78a85cbf357a444b8bff5304a87d91ad299
焕晨HChen <123531821+HChenX@users.noreply.github.com>
2025-03-01 20:53:35 +0800

    Update ShellTool.java

--- ShellTool.java
+++ ShellTool.java
@@ -681,15 +681,39 @@ public class ShellTool {
     }
 
     public interface IExecListener {
+        /**
+         * 标准输出。
+         *
+         * @param command  输入的命令
+         * @param outputs  执行后输出内容
+         * @param exitCode 退出码
+         */
         default void output(String command, String[] outputs, String exitCode) {
         }
 
+        /**
+         * 标准错误。
+         *
+         * @param command  输入的命令
+         * @param errors   报错内容
+         * @param exitCode 退出码
+         */
         default void error(String command, String[] errors, String exitCode) {
         }
 
+        /**
+         * 无 Root 时会执行的回调。
+         */
         default void notRoot(String exitCode) {
         }
 
+        /**
+         * 管道破裂时的回调，代码 Shell 流非正常终止。
+         *
+         * @param command 输入的命令
+         * @param errors  报错内容
+         * @param reason  崩溃原因
+         */
         default void brokenPip(String command, String[] errors, String reason) {
         }
     }
